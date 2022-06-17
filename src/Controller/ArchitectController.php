898efa55691a7299fc971ffaa82cf157f761c78f
gadax <58398018+gadax@users.noreply.github.com>
2022-06-17 08:46:39 +0200

    crud Architect

--- src/Controller/ArchitectController.php
+++ src/Controller/ArchitectController.php
@@ -2,22 +2,95 @@
 
 namespace App\Controller;
 
+use App\Entity\Architect;
+use App\Form\ArchitectType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/architect")
+ */
 class ArchitectController extends AbstractController
 {
     /**
-     * @Route("/architect", name="app_architect")
+     * @Route("/", name="app_architect_index", methods={"GET"})
      */
-    public function list()
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return Response('<html><body>ok</body></html>');
+        $architects = $entityManager
+            ->getRepository(Architect::class)
+            ->findAll();
+
+        return $this->render('architect/index.html.twig', [
+            'architects' => $architects,
+        ]);
     }
 
-    public function new()
+    /**
+     * @Route("/new", name="app_architect_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return Response('<html><body>ok</body></html>');
+        $architect = new Architect();
+        $form = $this->createForm(ArchitectType::class, $architect);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($architect);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_architect_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('architect/new.html.twig', [
+            'architect' => $architect,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_architect_show", methods={"GET"})
+     */
+    public function show(Architect $architect): Response
+    {
+        return $this->render('architect/show.html.twig', [
+            'architect' => $architect,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="app_architect_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Architect $architect, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ArchitectType::class, $architect);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_architect_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('architect/edit.html.twig', [
+            'architect' => $architect,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_architect_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Architect $architect, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$architect->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($architect);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_architect_index', [], Response::HTTP_SEE_OTHER);
     }
 }
